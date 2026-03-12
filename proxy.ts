963434ac46907e9cddfb09a11a60736edba3522c
RiciRic <ricivrdrm99@hotmail.de>
2026-03-12 22:59:07 +0100

    default language changed to en

--- proxy.ts
+++ proxy.ts
@@ -90,6 +90,6 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
   runtime: 'nodejs'
 };
